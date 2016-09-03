0a82d5a19f04373f43f615aff5732c490d899758
Peter Kok <peter@thehyve.nl>
2016-09-03 11:33:00 +0200

    Add multiple questions

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,17 +9,19 @@
   function MainController(toastr, $scope, Service) {
 
     $scope.name = '';
+    $scope.questions = ['How do you feel?', 'How does your back feel?', 'How do your hands feel?']
+    $scope.currentQuestion = 0;
 
     $scope.onUpVote = function() {
       console.log('up');
-      Service.registerVote('up', $scope.name);
-      toastr.success("Thanks for feedback! Good to hear:)");
+      Service.registerVote('up', $scope.name, $scope.currentQuestion);
+      $scope.currentQuestion++;
     };
 
     $scope.onDownVote = function() {
       console.log('down');
-      Service.registerVote('down', $scope.name);
-      toastr.success("Thanks for feedback!");
+      Service.registerVote('down', $scope.name, $scope.currentQuestion);
+      $scope.currentQuestion++;
     };
   }
 })();
