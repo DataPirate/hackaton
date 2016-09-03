34b89a216631381002875627cfad061c52ed082d
Peter Kok <peter@thehyve.nl>
2016-09-03 14:33:35 +0200

    Save name, tweak layout

--- src/app/settings/settings.controller.js
+++ src/app/settings/settings.controller.js
@@ -6,11 +6,14 @@
     .controller('SettingsController', SettingsController);
 
   /** @ngInject */
-  function SettingsController(toastr, $scope, Service) {
+  function SettingsController(toastr, $scope, Service, $state) {
     $scope.name = "";
     $scope.profession = "";
     $scope.professions = ['Programmer', 'Construction worker', 'Pirate', 'Librarian', 'Falconer', 'Magician'];
 
-
+    $scope.saveSettings = function() {
+      Service.setName($scope.name);
+      $state.go('home');
+    };
   }
 })();
